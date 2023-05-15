545712869d5f1c0ec732d511f64d7470b0670f55
ahmed-cheema <ahmed.cheema101@gmail.com>
2023-05-15 02:46:08 -0500

    Re-made requirements.txt

--- match/scraper.py
+++ match/scraper.py
@@ -8,10 +8,9 @@ from datetime import datetime
 import pandas as pd
 import numpy as np
 import bs4 as bs
-import requests
 import json
 
-import undetected_chromedriver as uc
+#import undetected_chromedriver as uc
 
 from .models import Match, Player
 
