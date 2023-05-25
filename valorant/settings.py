d88037b30859b074e481bf401020d211a1612953
Ahmed Cheema <ahmed.cheema101@gmail.com>
2023-05-24 23:19:18 -0500

    remove cache

--- valorant/settings.py
+++ valorant/settings.py
@@ -138,31 +138,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SESSIONS_ENGINE='django.contrib.sessions.backends.cache'
 
-def get_cache():
-  import os
-  try:
-    servers = os.environ['MEMCACHIER_SERVERS']
-    username = os.environ['MEMCACHIER_USERNAME']
-    password = os.environ['MEMCACHIER_PASSWORD']
-    return {
-      'default': {
-        'BACKEND': 'django_bmemcached.memcached.BMemcached',
-        # TIMEOUT is not the connection timeout! It's the default expiration
-        # timeout that should be applied to keys! Setting it to `None`
-        # disables expiration.
-        'TIMEOUT': None,
-        'LOCATION': servers,
-        'OPTIONS': {
-          'username': username,
-          'password': password,
-        }
-      }
-    }
-  except:
-    return {
-      'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
-      }
-    }
-
-CACHES = get_cache()
